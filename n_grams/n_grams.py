8b2ca94637f957b55b0af482cd5e34d97c8fd6c5
ChrisDemSas <christopher.sastropranoto@gmail.com>
2024-10-14 21:21:26 +0800

    Finished process_text

--- n_grams/n_grams.py
+++ n_grams/n_grams.py
@@ -54,6 +54,7 @@ class NGrams:
         for word in data:
             if not (word in self.words):
                 self.words.append(word)
+                self.vector.append(0)
             else:
                 self.vector.append(0)
     
@@ -73,7 +74,10 @@ class NGrams:
 
             if not (curr_sentence in self.data):
                 self.data[curr_sentence] = self.vector
-    
+            
+            word_index = self.words.index(word)
+            self.data[curr_sentence][word_index] += 1           
+
     def process_text(self) -> list:
         """Initialize the word vector using words in self.words.
         """
@@ -81,8 +85,6 @@ class NGrams:
         self.process_sentence()
         self.process_words()
         self.process_data()
-
-        return self.data
         
     def predict(self, fragment: str) -> None:
         """Take in a text fragment and predict the next word.
@@ -95,10 +97,5 @@ if __name__ == "__main__":
     text = """I don’t like cats and they don’t like me. I used to be allergic to them and I would get stuffed up and have hives. That doesn’t seem to happen anymore. But I still don’t like them. I lived with 3 cats that were not good at peeing in the litter box. They seemed to find something important to me and pee on it. Most of the time they peed on photographs or papers that would be ruined. Cats also bring fleas into the house. There is nothing worse than having to flea dip cats and also flea bomb a home. That is why I don’t like cats.
     """
 
-    n = NGrams(3, text)
-    n.process_sentence()
-    n.process_words()
-    n.process_data()
-    #t = n.process_text()
-    print(n.data)
+
 
