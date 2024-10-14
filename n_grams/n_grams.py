b0049235c42f9abc5b85eef55f51fac72f529309
ChrisDemSas <christopher.sastropranoto@gmail.com>
2024-10-14 21:10:02 +0800

    Process Words Added

--- n_grams/n_grams.py
+++ n_grams/n_grams.py
@@ -23,16 +23,17 @@ class NGrams:
         self.data = {}
         self.words = []
         self.vector = []
+        self.original_text = text
         self.text = ""
     
-    def process_sentence(self, text: str) -> str:
+    def process_sentence(self) -> str:
         """Take in a text and remove all the full stops.
         
         Attributes:
             text: Take in a text and remove all full stops.
         """
 
-        data = text.split()
+        data = self.original_text.split()
         final = []
 
         for word in data:
@@ -41,18 +42,47 @@ class NGrams:
             else:
                 final.append(word)
         
-        self.text = " ".join(final).lower()
+        text = " ".join(final)
+        self.text = text.lower()
 
-    def initialize(self, text: str) -> None:
+    def process_words(self) -> None:
         """Take in a body of text and then populate self.data with keys and empty list.
-
-        Attributes:
-            text: The text to be processed.
         """
-        
-    def process_text(self, text: str) -> list:
+
+        data = self.text.split()
+
+        for word in data:
+            if not (word in self.words):
+                self.words.append(word)
+            else:
+                self.vector.append(0)
+    
+    def process_data(self) -> None:
+        """Process the text by adding (n-1) - grams, with the word vectors.
+        """
+
+        data = self.text.split()
+
+        for index, word in enumerate(data):
+
+            if index < self.n:
+                continue
+            
+            curr_sentence = data[index - self.n:index - 1]
+            curr_sentence = " ".join(curr_sentence)
+
+            if not (curr_sentence in self.data):
+                self.data[curr_sentence] = self.vector
+    
+    def process_text(self) -> list:
         """Initialize the word vector using words in self.words.
         """
+
+        self.process_sentence()
+        self.process_words()
+        self.process_data()
+
+        return self.data
         
     def predict(self, fragment: str) -> None:
         """Take in a text fragment and predict the next word.
@@ -65,7 +95,10 @@ if __name__ == "__main__":
     text = """I don’t like cats and they don’t like me. I used to be allergic to them and I would get stuffed up and have hives. That doesn’t seem to happen anymore. But I still don’t like them. I lived with 3 cats that were not good at peeing in the litter box. They seemed to find something important to me and pee on it. Most of the time they peed on photographs or papers that would be ruined. Cats also bring fleas into the house. There is nothing worse than having to flea dip cats and also flea bomb a home. That is why I don’t like cats.
     """
 
-    n = NGrams(3)
-    t = n.remove_full_stops(text)
-    print(t)
+    n = NGrams(3, text)
+    n.process_sentence()
+    n.process_words()
+    n.process_data()
+    #t = n.process_text()
+    print(n.data)
 
